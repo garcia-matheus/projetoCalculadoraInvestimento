9cd77b669decae92d5243aee37154a128479b95b
Matheus Garcia <matheuscor1903@gmail.com>
2024-11-27 13:49:36 -0300

    mesma coisa da ultima atualizaçao, porem com o main adicionado

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import { generateReturnsArray } from "./src/investmentGoals";
 
 const form = document.getElementById('investment-form');
-const calculateButton = document.getElementById('calculate-results');
+//const calculateButton = document.getElementById('calculate-results');
 
 function renderProgression(evt) {
     evt.preventDefault();
-    const startingAmount = Number(form['starting-amount'].value)
-    // const startingAmount = Number(document.getElementById('starting-amount').value);
+    // const startingAmount = Number(form['starting-amount'].value)
+    const startingAmount = Number(document.getElementById('starting-amount').value);
     const additionalContribution = Number(document.getElementById('additional-contribution').value);
     const timeAmount = Number(document.getElementById('time-amount').value);
     const timeAmountPeriod = document.getElementById('time-amount-period').value;
@@ -20,5 +20,5 @@ function renderProgression(evt) {
 }
 
 form.addEventListener('submit', renderProgression)
-calculateButton.addEventListener('click', renderProgression)
+//calculateButton.addEventListener('click', renderProgression)
 
